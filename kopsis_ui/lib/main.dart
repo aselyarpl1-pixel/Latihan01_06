bbe378443eaf4c24159c8baccf603afa56580e07
aselya-coder <aselyamagangjogja@gmail.com>
2026-08-21 07:28:33 +0700

    LKM-3 kartu barang

--- kopsis_ui/lib/main.dart
+++ kopsis_ui/lib/main.dart
@@ -9,27 +9,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+  
+    //Variabel stok
+    int stok = 40;
+
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: const Text('Koperasi Sekolah')),
-          body: Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Buku Tulis'),
-
-                //memberi jarak antara nama barang dan harga
-                const SizedBox(height: 8),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween, //mengatur jarak harga agar tidak berdekatan
-                  children: const [
-                    Text('Anggota: Rp3.000'),
-                    Text('Umum: Rp3.500'),
-                  ],
+          appBar: AppBar(
+            title: const Text('Koperasi Sekolah')
+            ),
+          body: Card (
+            margin: const EdgeInsets.all(12),
+            child: ListTile(
+              //icon barang
+              leading: const Icon(Icons.inventory_2),
+              //menebalkan nama barang
+              title: const Text(
+                'Buku Tulis',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-              ],
+              //harga anggota dan umum
+              subtitle: const Text('Anggota Rp3.000 | Umum Rp3.500'),
+              //stok barang
+              trailing: Text(
+                // ignore: prefer_interpolation_to_compose_strings
+                'Stok' + stok.toString(),
+                style: TextStyle(color: stok == 0 ? Colors.red : Colors.black),
+              ),
             ),
           ),
         ),
